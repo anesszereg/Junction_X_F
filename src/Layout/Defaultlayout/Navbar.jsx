0cee08cae286c7fc76a7c5416fd86620e976b9ad
sido200 <93400204+sido200@users.noreply.github.com>
2023-10-05 22:59:56 +0100

    sidali

--- src/Layout/Defaultlayout/Navbar.jsx
+++ src/Layout/Defaultlayout/Navbar.jsx
@@ -1,8 +1,60 @@
-
-
+import { Box } from "@mui/material"
+import { BsMoon,BsSearch } from "react-icons/bs";
+import { LuBell } from "react-icons/lu";
+import { GoGlobe } from "react-icons/go";
+import image from '../../assets/profil.png'
 function Navbar() {
   return (
-    <div>Navbar</div>
+   <Box sx={{
+    display:"flex",
+    alignItems:"center",
+    justifyContent:"space-between",
+    margin:" 20PX 100px",
+    padding:"0 40px",
+    position:"absolute",
+    width:"calc(100% - 280px)",
+    height:"80px",
+    top:"0",
+    border:"1px solid #E5E5E5 ",
+    borderRadius:"8px"
+   
+    
+   }}>
+   <Box sx={{
+    display:"flex",
+    alignItems:"center",
+    gap:"20px"
+   }}>
+     <BsSearch style={{ width:"25px",height:"25px ",color:"#C8C8C8"}}/>
+   <input type="search" name="" id="" placeholder="Search ( Ctrl + / ) " 
+    style={{
+      border:"none",
+        color: '#C8C8C8',
+        fontFamily: 'Inter',
+        fontSize: '14px',
+        fontStyle: 'normal',
+        fontWeight: 500,
+        lineHeight: 'normal',
+      
+      
+    }}
+   />
+  
+   </Box>
+     <Box sx={{
+      display:"flex",
+      alignItems:"center",
+      justifyContent:"space-between",
+      gap:"20px",
+     }}>
+      <GoGlobe style={{ width: '22px', height: '22px' }}/>
+<BsMoon  style={{ width: '22px', height: '22px' }}/>
+<LuBell style={{ width: '22px', height: '22px' }}/>
+
+<img src={image} alt="" style={{ width: '48px', height: '48px',borderRadius:"50%", backgroundColor:"#ded1ff", display:"flex",alignItems:"center",
+      justifyContent:"center", }} />
+     </Box>
+   </Box>
   )
 }
 
