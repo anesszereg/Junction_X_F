142b563c5fbfe9c418d80968e569801740ac3cb6
sido200 <93400204+sido200@users.noreply.github.com>
2023-10-07 04:22:07 +0100

    sidali

--- src/Components/LandingPage/Secound.jsx
+++ src/Components/LandingPage/Secound.jsx
@@ -4,18 +4,27 @@ import girl from "../../assets/girl.jpg"
 import educ from "../../assets/educ.png"
 import text3 from "../../assets/text3.png"
 import uranus from "../../assets/uranus.png"
+import uranus1 from "../../assets/uranus1.png"
+import first from "../../assets/first.png"
+import easy from "../../assets/easy.png"
+import LastT from "../../assets/LastT.png"
+import planet from "../../assets/planet.png"
+import use from "../../assets/use.png"
+import  friendly from "../../assets/friendly.png"
 import { IoPlayCircleOutline } from "react-icons/io5";
+
 
 
 const Secound = () => {
     return (
         <Box sx={{ width: '100%',
-        height: '2533px',
+        height: '3033px',
+      
         flexShrink: 0,display:"flex",
         flexDirection:"column",
         alignItems:"center",
         borderRadius: '80px 80px 0px 0px',
-        gap:"200px",
+        gap:"250px",
         paddingTop:"100px" ,     background: '#242424',marginTop:"-100px"}}>
   <Box sx={{position:"relative"}}>
     <img src={educ} alt="" style={{position:"absolute",left:"290px", width: '300px',
@@ -38,6 +47,15 @@ const Secound = () => {
   
   </Box>
 <Grid/>
+<Box sx={{display:"flex",alignItems:"flex-start",flexDirection:"column",gap:"10px"}}>
+  <Box sx={{display:"flex",alignItems:"center",gap:"10px"}}><img src={first} alt="" />
+  <img src={easy} alt="" />
+  <img src={use} alt="" />
+  </Box>
+  <img src={LastT} alt="" />
+  <img src={planet} alt=""  style={{position:"absolute",right:"10px",bottom:"-1950px"}}/>
+  
+</Box>
 <Box sx={{width: '1200px',
   height: 'auto',
   flexShrink: 0,
@@ -45,9 +63,10 @@ const Secound = () => {
   position: 'relative'}}>
     <img src={girl} alt="" style={{width: '100%',
     height: '100%', borderRadius: '60px',
-    objectFit: 'fill',}} />
+    objectFit: 'fill',zIndex:"1", position:"relative"}} />
     <IoPlayCircleOutline style={{
   position: 'absolute',
+  zIndex:"2",
   left: '45%',
   top: '45%',
   fill: 'white', // Ceci est généralement utilisé pour les icônes SVG
@@ -55,6 +74,8 @@ const Secound = () => {
   height: '120px',
   width: '120px',
 }}/>
+<img src={friendly} alt=""  style={{position:"absolute" ,right:"-100px", bottom:"-150px",zIndex:"2"}}/>
+<img src={uranus1} alt=""  style={{position:"absolute" ,left:"-150px", top:"-150px",zIndex:"0"}}/>
 </Box>
         </Box>
     );
